81d5875c2417cb249eb132b8da1bb88793a10c4a
grey <iamwotou@gmail.com>
2023-09-14 18:00:56 -0500

    config for deploy on github

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Grey's Log",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gr-grey.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
